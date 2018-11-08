3db7d43f01f85e88dd89f1b66f3bdae3ac68e206
bltnico <bellot.n@gmail.com>
2018-11-08 17:03:06 +0100

    Remove useless console.lo

--- App.js
+++ App.js
@@ -4,10 +4,6 @@ import { Provider } from 'react-redux';
 import store from './src/store';
 import RootNavigator from './src/components/navigators/RootNavigator';
 
-import fetchUsers from './src/api';
-
-fetchUsers().then(users => console.log(users));
-
 class App extends Component<{}> {
   render() {
     return (
